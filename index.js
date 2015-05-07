1f55122902ab134f6213d71a39e5bb5b1920ea91
quasiunafantasia <agoldfarb@ukr.net>
2015-05-07 15:33:29 +0300

    asd

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ var pg = require("pg-promise")(),
 Q  = require("q"),
 CN = "postgres://postgres:root@localhost:5432/lab2";
 var db = pg(CN);
-
-db.tx(function(transaction){
-  return transaction.query("insert into planes values ($1, $2)", [1, 1])
-  .then(function() {
-    return transaction.query("prepare foo AS insert into hotels values (1, 1)");
-  })
-  .then(function() {
+() {
      console.log('success');
      return transaction.one('COMMIT PREPARED foo');
 

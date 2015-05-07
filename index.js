300c8fe4e5a6150b5a5e743140df3997183152d3
quasiunafantasia <agoldfarb@ukr.net>
2015-05-07 15:28:59 +0300

    transaction without prepared statement

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ CN = "postgres://postgres:root@localhost:5432/lab2";
 var db = pg(CN);
 
 db.tx(function(transaction){
-  return transaction.query("insert into planes values ($1, $2)", [1, 1])
-  .then(function() {
-    return transaction.query("prepare foo AS insert into hotels values (1, 1)");
-  })
-  .then(function() {
-     console.log('success');
-     return transaction.one('COMMIT PREPARED foo');
-
-  }, function(reason) {
-   console.error('err');
-    console.log(reason);
-   return transaction.one('ROOLBACK PREPARED foo')
-});
+  var q1 = transaction.query("insert into planes values ($1, $2)", [1, 1]);
+  var q2 = transaction.query("insert into hotels values ($1, $2)", [1, 1]);
+  //return Q.all([q1, q2]);
+  throw new err('asd');
+})
+.then(function(data) {
+  console.log("success");
+})
+.done(null, function(reason) {
+  console.error('err');
+  console.log(reason);
 });

866b6cb73712e9d3ce4879b685a0c957f1ee5c6b
quasiunafantasia <agoldfarb@ukr.net>
2015-05-07 15:32:59 +0300

    asd

--- index.js
+++ index.js
@@ -6,15 +6,4 @@ var db = pg(CN);
 db.tx(function(transaction){
   return transaction.query("insert into planes values ($1, $2)", [1, 1])
   .then(function() {
-    return transaction.query("prepare foo AS insert into hotels values (1, 1)");
-  })
-  .then(function() {
-     console.log('success');
-     return transaction.one('COMMIT PREPARED foo');
 
-  }, function(reason) {
-   console.error('err');
-    console.log(reason);
-   return transaction.one('ROOLBACK PREPARED foo')
-});
-});
